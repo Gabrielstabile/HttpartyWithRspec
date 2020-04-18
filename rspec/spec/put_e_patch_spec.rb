42a9ad11e18da87140b044452044868d3a183187
Gabrielstabile <gabrielstabilee@gmail.com>
2020-04-18 13:33:36 -0300

    finishing expects

--- rspec/spec/put_e_patch_spec.rb
+++ rspec/spec/put_e_patch_spec.rb
@@ -1,7 +1,9 @@
 describe "fazer put e patch" do
     len = SecureRandom.alphanumeric(5)
-    
+
     context "Alterando dados com" do
+
+        @emailAntes = Contato.get("/contacts/872").parsed_response['data']['attributes']['email']
     
         it 'put' do
             @body = {
@@ -15,19 +17,24 @@ describe "fazer put e patch" do
             "state": "Minas Gerais",
             "city": "Belo Horizonte"
         }.to_json
-
+        
         @requisicaoPut = Contato.put("/contacts/872", body: @body)
-        puts @requisicaoPut
+        @emailAtual = Contato.get("/contacts/872").parsed_response['data']['attributes']['email']
+        expect(@emailAtual).not_to eq @emailAntes
+        
         end
 
         it 'patch' do
+            @nameAntes = Contato.get("/contacts/871").parsed_response['data']['attributes']['name']
+
             @body = {
             "id": "871",
-            "name": "pequenoteste"
+            "name": "#{len}"
             }.to_json
 
             @requisicaoPatch = Contato.patch("/contacts/871", body: @body)
-            # puts @requisicaoPatch
+            @nameAtual = Contato.get("/contacts/871").parsed_response['data']['attributes']['name']
+            expect(@nameAtual).not_to eq @emailAntes
         end
     end
 end
